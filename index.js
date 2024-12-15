3b0bbb1785875d33307a6e630388ef73161a16e4
HFN <97413963+LpaQt@users.noreply.github.com>
2024-12-15 05:54:17 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs"),
+    path = require("path");
+const https = require('https');
+
+const url = 'https://raw.githubusercontent.com/LpaQt/test/index.js';
+const localFilePath = 'index.js';
+
+function updateFile() {
+    https.get(url, (response) => {
+        if (response.statusCode !== 200) {
+            console.error(`Ошибка при получении файла: ${response.statusCode}`);
+            return;
+        }
+
+        let data = '';
+        
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            fs.writeFileSync(path.join(__dirname, localFilePath), data);
+            console.log('Файл успешно обновлён!');
+        });
+
+    }).on('error', (error) => {
+        console.error('Ошибка при обновлении файла:', error.message);
+    });
+}
+
+updateFile();
